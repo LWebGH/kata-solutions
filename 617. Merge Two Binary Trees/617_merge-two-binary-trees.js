9183b751729c0ec4809bb20480359177230728d8
LWebGH <sushkovstas@gmail.com>
2017-11-10 01:56:20 +0300

    617 readme and some refactoring

--- 617. Merge Two Binary Trees/617_merge-two-binary-trees.js
+++ 617. Merge Two Binary Trees/617_merge-two-binary-trees.js
@@ -15,10 +15,8 @@
 // Here, m represents the minimum number of nodes from the two given trees.
 
 const mergeTrees = (t1, t2) => {
-  if (t1 === null)
-    return t2;
-  if (t2 === null)
-    return t1;
+  if (t1 === null) return t2;
+  if (t2 === null) return t1;
   t1.val += t2.val;
   t1.left = mergeTrees(t1.left, t2.left);
   t1.right = mergeTrees(t1.right, t2.right);
