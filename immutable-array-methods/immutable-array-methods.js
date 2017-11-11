590ce301510bed5b579bf22216f9428d59fe5bd7
LWebGH <sushkovstas@gmail.com>
2017-11-11 18:30:32 +0300

    immutable arrays more tests added

--- immutable-array-methods/immutable-array-methods.js
+++ immutable-array-methods/immutable-array-methods.js
@@ -1,8 +1,8 @@
 
 const clone = (array) => [...array];
 const push = (...items) => (array) => [...array, ...items];
-const unshift = (...items) => (array) => [...items, ...array];
 const pop = (array) => array.slice(0, -1);
+const unshift = (...items) => (array) => [...items, ...array];
 const shift = (array) => array.slice(1);
 const sort = (func) => (array) => [...array].sort(func);
 const del = (i) => (array) => [...array.slice(0, i), ...array.slice(i + 1)];
--- immutable-array-methods/immutable-array-methods.test.js
+++ immutable-array-methods/immutable-array-methods.test.js
@@ -12,28 +12,58 @@ describe('clone', () => {
 });
 
 describe('push', () => {
-  test('push(\'a\', \'b\')(immutableArray) => [1, 2, 3, 1, \'a\', \'b\']', () => {
+  test('push(\'a\', \'b\')(immutableArray) => [1, 2, 3, 1, \'a\', \'b\'])', () => {
     expect(push('a', 'b')(immutableArray)).toEqual([1, 2, 3, 1, 'a', 'b']);
   });
-  test('expect(immutableArray).toEqual([1, 2, 3, 1])', () => {
-    expect(immutableArray).toEqual([1, 2, 3, 1]);
+});
+
+describe('pop', () => {
+  test('pop(immutableArray) => toEqual([1, 2, 3])', () => {
+    expect(pop(immutableArray)).toEqual([1, 2, 3]);
   });
 });
 
 describe('unshift', () => {
-  test('unshift(\'a\', \'b\')(immutableArray) => toEqual([\'a\', \'b\', 1, 2, 3, 1]', () => {
+  test('unshift(\'a\', \'b\')(immutableArray) => toEqual([\'a\', \'b\', 1, 2, 3, 1])', () => {
     expect(unshift('a', 'b')(immutableArray)).toEqual(['a', 'b', 1, 2, 3, 1]);
-  });
-  test('expect(immutableArray).toEqual([1, 2, 3, 1])', () => {
-    expect(immutableArray).toEqual([1, 2, 3, 1]);
   });
 });
 
 describe('shift', () => {
-  test('shift(immutableArray) => toEqual(2, 3, 1]', () => {
+  test('shift(immutableArray) => toEqual([2, 3, 1])', () => {
     expect(shift(immutableArray)).toEqual([2, 3, 1]);
   });
-  test('expect(immutableArray).toEqual([1, 2, 3, 1])', () => {
+});
+
+describe('sort', () => {
+  test('sort()(immutableArray) => toEqual([1, 1, 2, 3])', () => {
+    expect(sort()(immutableArray)).toEqual([1, 1, 2, 3]);
+  });
+  test('sort((a, b) => b - a)(immutableArray) => toEqual([3, 2, 1, 1])', () => {
+    expect(sort((a, b) => b - a)(immutableArray)).toEqual([3, 2, 1, 1]);
+  });
+});
+
+describe('del', () => {
+  test('expect(del(0)(immutableArray)).toEqual([2, 3, 1])', () => {
+    expect(del(0)(immutableArray)).toEqual([2, 3, 1]);
+  });
+  test('expect(del(1)(immutableArray)).toEqual([1, 3, 1])', () => {
+    expect(del(1)(immutableArray)).toEqual([1, 3, 1]);
+  });
+});
+
+describe('splice', () => {
+  test('splice(0, 0, 12)(immutableArray)).toEqual([12, 1, 2, 3, 1])', () => {
+    expect(splice(0, 0, 12)(immutableArray)).toEqual([12, 1, 2, 3, 1]);
+  });
+  test('splice(1, 3, 3, 3, 7)(immutableArray)).toEqual([1, 3, 3, 7])', () => {
+    expect(splice(1, 3, 3, 3, 7)(immutableArray)).toEqual([1, 3, 3, 7]);
+  });
+});
+
+describe('immutablility test', () => {
+  test('immutableArray.toEqual([1, 2, 3, 1])', () => {
     expect(immutableArray).toEqual([1, 2, 3, 1]);
   });
 });
