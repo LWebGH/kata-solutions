f8d1819efca881ec528e2f883694200dc3c674be
LWebGH <sushkovstas@gmail.com>
2017-11-17 15:59:37 +0300

    quick-sort.js

--- A_Quick Sort/quick-sort.js
+++ A_Quick Sort/quick-sort.js
@@ -1,15 +1,17 @@
 // Complexity: O(n log n)
 
 const quickSort = array => {
+  const arrayCopy = [...array];  // O(n log n + n) ??? 
+  
+  const sort = (copy) => {
+    if (array.length <= 1) return array;
+    const pivot = copy.splice(Math.floor(copy.length / 2), 1)[0];
+    const lesser = copy.filter(item => item < pivot);
+    const equalnGreater = copy.filter(item => item >= pivot);
+    return [...quickSort(lesser), pivot, ...quickSort(equalnGreater)];
+  };
 
-  if (array.length <= 1) return array;
-
-  const copy = [...array];
-  const pivot = copy.splice(Math.floor(copy.length / 2), 1)[0];
-  const lesser = copy.filter(item => item < pivot);
-  const equalnGreater = copy.filter(item => item >= pivot);
-
-  return [...quickSort(lesser), pivot, ...quickSort(equalnGreater)];
+  return sort(arrayCopy);
 };
 
 export default quickSort;
