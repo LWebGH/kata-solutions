2b1363676d8659248e0d0fce378f1cd1f85f7d97
LWebGH <sushkovstas@gmail.com>
2017-11-22 17:34:02 +0300

    5.high-ordered-funtions ageDifference()

---
+++ B_eloquent-javascript/5.high-ordered-functions/age-difference.js
@@ -0,0 +1,19 @@
+const tableOfNames = (data) => {
+  const result = {};
+  data.forEach(persone => result[persone["name"]] = persone);
+  return result;
+};
+
+const ageDifference = (data) => {
+  const persons = tableOfNames(data);
+  const ageStorage = [];
+  data.forEach(child => {
+    if (persons[child["mother"]]) {
+      let mother = persons[child["mother"]];
+      ageStorage.push(child["born"] - mother["born"]);
+    };
+  });
+  return ageStorage.reduce((sum, item) => sum + item) / ageStorage.length;
+};
+
+export { tableOfNames, ageDifference };
--- B_eloquent-javascript/5.high-ordered-functions/high-ordered-functions.test.js
+++ B_eloquent-javascript/5.high-ordered-functions/high-ordered-functions.test.js
@@ -1,4 +1,6 @@
+import { ancestryFile } from './ancestry';
 import { flattering } from './flattering';
+import { tableOfNames, ageDifference } from './age-difference';
 
 describe('One dimensional array tests', () => {
   test('flattering([[1, 2, 3], [4, 5], [6]]) => [ 1, 2, 3, 4, 5, 6 ]', () => {
@@ -6,5 +8,11 @@ describe('One dimensional array tests', () => {
   });
   test('flattering([[1, 2, 3], [4, 5, ["another", "dimension", ["of", "arrays", "inside", "others"]]], [6]]) => [ 1, 2, 3, 4, 5, "another", "dimension", "of", "arrays", "inside", "others", 6 ]', () => {
     expect(flattering([[1, 2, 3], [4, 5, ["another", "dimension", ["of", "arrays", "inside", "others"]]], [6]])).toEqual([1, 2, 3, 4, 5, "another", "dimension", "of", "arrays", "inside", "others", 6]);
+  });
+});
+
+describe('Age difference tests', () => {
+  test('ageDifference(ancestryFile).toFixed(2)).toEqual(31.22)', () => {
+    expect(ageDifference(ancestryFile).toFixed(2)).toEqual('31.22');
   });
 });
